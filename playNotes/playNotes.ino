1a60aabdd93ae8043c186f3c69bfc2953f14eed7
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2015-10-13 17:12:11 -0400

    Put in the song Fur Elise

--- playNotes/playNotes.ino
+++ playNotes/playNotes.ino
@@ -30,21 +30,42 @@ byte tempWaveIndex = 0;
 byte tempWaveType = PAUSE;
 
 //define notes
-const short C = 2093;
-const short D = 2349;
-const short E = 2637;
-const short F = 2793;
-const short G = 3135;
-const short A = 3520;
-const short B = 3951;
+const short C_6 = 1046;
+const short D_6 = 1174;
+const short D_6s = 1244;
+const short E_6 = 1318;
+const short F_6 = 1396;
+const short G_6 = 1567;
+const short G_6s = 1661;
+const short A_6 = 1760;
+const short B_6 = 1975;
+
+const short C_7 = 2093;
+const short D_7 = 2349;
+const short D_7s = 2489;
+const short E_7 = 2637;
+const short F_7 = 2793;
+const short G_7 = 3135;
+const short A_7 = 3520;
+const short B_7 = 3951;
+
 const short HIGHC = 4186;
 
 //first note in notes and duration array is a sentinel value that is not actually played
 //song will loop once it reaches the end of the array
-short notes[] = {0,C,D,E,F,G,A,B,HIGHC};
+//short notes[] = {0, C, D, E, F, G, A, B, HIGHC};
+
+// Fur Elise notes
+short notes[] = {E_7, D_7s, E_7, D_7s, E_7, B_6, D_7, C_7, A_6, 0, C_6, E_6, A_6, B_6, 0, E_6, G_6s, B_6, C_7, 0, E_6, E_7, D_7s, E_7, D_7s, E_7, B_6, D_7, C_7, A_6, 0, C_6, E_6, A_6, B_6, 0, E_6, C_7, B_6, A_6};
+
 //short notes[] = {0,C/6,D/6,E/6,F/6,G/6,A/6,B/6,HIGHC/6};
-int duration[] = {0,100,100,100,100,100,100,100,100}; // in .01s increments
-int songLen = sizeof(notes)/sizeof(short);
+
+//int duration[] = {0, 100, 100, 100, 100, 100, 100, 100, 100}; // in .01s increments
+
+// Fur Elise rhythm (NOTE: multiplied later in set up for right timing)
+int duration[] = {1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 4};
+
+int songLen = sizeof(notes) / sizeof(short);
 int songIndex = 0;
 int noteDuration = 0;
 
@@ -52,8 +73,12 @@ const long DEBOUNCE_TIME = 150;
 long button0PressedTime = 0;
 long button1PressedTime = 0;
 
-void setup(){
+void setup() {
   Serial.begin(9600);
+
+  for (int i = 0; i < songLen; i++) {
+    duration[i] = duration[i]*100;
+  }
 
   //set input output modes of pins that write to the DAC
   DDRB = 0B11111111; //set pins 8 to 13 as outputs
@@ -80,55 +105,55 @@ void setup(){
   sei();
 }
 
-void buildSinLookup(){
-  for (int i=0; i<LENGTH; i++) { // Step across wave tables
-   float v = (AMP*sin((PI2/LENGTH)*i)); // Compute value
-   sinWave[i] = reverse(byte(v+OFFSET)); // Store value as integer
+void buildSinLookup() {
+  for (int i = 0; i < LENGTH; i++) { // Step across wave tables
+    float v = (AMP * sin((PI2 / LENGTH) * i)); // Compute value
+    sinWave[i] = reverse(byte(v + OFFSET)); // Store value as integer
   }
 }
 
-void buildSquareLookup(){
-  for (int i=0; i<LENGTH/2; i++) {
-   squareWave[i] = reverse(byte(255)); // First half of square wave is high
+void buildSquareLookup() {
+  for (int i = 0; i < LENGTH / 2; i++) {
+    squareWave[i] = reverse(byte(255)); // First half of square wave is high
   }
-  for (int i=LENGTH/2; i<LENGTH; i++) {
-   squareWave[i] = reverse(byte(0)); // Second half is low
-  }
-}
-
-void buildSawLookup(){
-  for (int i=0; i<LENGTH; i++) {
-   float v = 255 * i * 1.0 / LENGTH;
-   sawWave[i] = reverse(byte(v));
+  for (int i = LENGTH / 2; i < LENGTH; i++) {
+    squareWave[i] = reverse(byte(0)); // Second half is low
   }
 }
 
-void buildBitShiftTables(){
-    for(int i = 0; i < 256; i++){
-      shiftLeftSix[i] = i << 6;
-      shiftRightTwo[i] = i >> 2;
-    }
+void buildSawLookup() {
+  for (int i = 0; i < LENGTH; i++) {
+    float v = 255 * i * 1.0 / LENGTH;
+    sawWave[i] = reverse(byte(v));
+  }
+}
+
+void buildBitShiftTables() {
+  for (int i = 0; i < 256; i++) {
+    shiftLeftSix[i] = i << 6;
+    shiftRightTwo[i] = i >> 2;
+  }
 }
 
 //timer two takes care of changing frequencies, checking at 100 Hz
-void initializeTimerTwoInterrupt(){
+void initializeTimerTwoInterrupt() {
   TCCR2A = 0;// set entire TCCR1A register to 0
   TCCR2B = 0;// same for TCCR1B
   TCNT2  = 0;//initialize counter value to 0
   //PRESCALER is x1024
   //Set compare register
-  OCR2A = (16000000L) / (100*1024) - 1;//(must be <256)
+  OCR2A = (16000000L) / (100 * 1024) - 1; //(must be <256)
   // turn on CTC mode
   TCCR2B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler
   TCCR2B |= (1 << CS12) | (1 << CS10);
   // enable timer compare interrupt
   TIMSK2 |= (1 << OCIE1A);
-} 
+}
 
 
 //timer one outputs waveforms
-void initializeTimerOneInterrupt(){
+void initializeTimerOneInterrupt() {
   TCCR1A = 0;// set entire TCCR1A register to 0
   TCCR1B = 0;// same for TCCR1B
   TCNT1  = 0;//initialize counter value to 0
@@ -136,7 +161,7 @@ void initializeTimerOneInterrupt(){
   // turn on CTC mode
   TCCR1B |= (1 << WGM12);
   // Set CS10 bit for 1 prescaler
-  TCCR1B |= (1 << CS10);  
+  TCCR1B |= (1 << CS10);
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
 }
@@ -147,7 +172,7 @@ void initializeTimerZeroInterrupt() {
   TCNT0 = 0;
   //PRESCALER is x1024
   //Set compare register
-  OCR0A = (16000000L) / (100*1024) - 1;//(must be <256)
+  OCR0A = (16000000L) / (100 * 1024) - 1; //(must be <256)
   // turn on CTC mode
   TCCR0B |= (1 << WGM12);
   // Set CS10 and CS12 bits for 1024 prescaler
@@ -156,18 +181,18 @@ void initializeTimerZeroInterrupt() {
   TIMSK0 |= (1 << OCIE1A);
 }
 
-void setTimerOneInterrupt(short compareReg){
+void setTimerOneInterrupt(short compareReg) {
   //Set compare register
   OCR1A = compareReg;//(must be <65536)
 }
 
-short waveFreqToCompareReg(long waveFreq){
+short waveFreqToCompareReg(long waveFreq) {
   long interruptFreq = waveFreq * (long)LENGTH;
   short compareReg = (16000000L) / (interruptFreq) - 1;
   return compareReg;
 }
 
-void writeByte(byte val){
+void writeByte(byte val) {
   byte portDbyte = shiftLeftSix[val];
   byte portBbyte = shiftRightTwo[val];
   PORTD = portDbyte;
@@ -176,15 +201,15 @@ void writeByte(byte val){
 
 byte reverse(byte inb) {
   //reverse function taken from http://stackoverflow.com/questions/2602823/in-c-c-whats-the-simplest-way-to-reverse-the-order-of-bits-in-a-byte
-   byte b = inb;
-   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-   return b;
+  byte b = inb;
+  b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+  b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+  b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+  return b;
 }
 
-void changeWaveType(){
-  switch(waveType){
+void changeWaveType() {
+  switch (waveType) {
     case PAUSE:
       break;
     case SIN:
@@ -199,7 +224,7 @@ void changeWaveType(){
   }
 }
 
-void togglePause(){
+void togglePause() {
   if (waveType == PAUSE) {
     waveType = tempWaveType;
     waveIndex = tempWaveIndex;
@@ -211,27 +236,27 @@ void togglePause(){
   }
 }
 
-void checkPauseButton(){
+void checkPauseButton() {
   if (digitalRead(A0)) {
     button0PressedTime++;
-    if (button0PressedTime >= DEBOUNCE_TIME){
+    if (button0PressedTime >= DEBOUNCE_TIME) {
       togglePause();
       button0PressedTime = 0;
-    }    
+    }
   } else {
     button0PressedTime = 0;
   }
 }
 
-void checkWaveChangeButton(){
+void checkWaveChangeButton() {
   if (digitalRead(A1)) {
     button1PressedTime++;
-    if (button1PressedTime >= DEBOUNCE_TIME){
-      if(waveType != PAUSE){
+    if (button1PressedTime >= DEBOUNCE_TIME) {
+      if (waveType != PAUSE) {
         changeWaveType();
         button1PressedTime = 0;
-      }    
-    }    
+      }
+    }
   } else {
     button1PressedTime = 0;
   }
@@ -244,19 +269,19 @@ ISR(TIMER0_COMPA_vect) {
 }
 
 //Timer one writes out waves to DAC
-ISR(TIMER1_COMPA_vect){//timer1 interrupt writes bytes onto D6 to D13
+ISR(TIMER1_COMPA_vect) { //timer1 interrupt writes bytes onto D6 to D13
   //use the bitmask B00011111 to take the index mod32 and quickly index into the array
   writeByte(wave[waveType][B00011111 & waveIndex]);
   waveIndex++;
 }
 
 //100 Hz timer two interrupt changes frequencies
-ISR(TIMER2_COMPA_vect){
-  if(songIndex >= songLen){
-   songIndex = 0; 
+ISR(TIMER2_COMPA_vect) {
+  if (songIndex >= songLen) {
+    songIndex = 0;
   }
   noteDuration++;
-  if(noteDuration > duration[songIndex]){
+  if (noteDuration > duration[songIndex]) {
     //go to the next note
     noteDuration = 0;
     songIndex++;
@@ -264,5 +289,5 @@ ISR(TIMER2_COMPA_vect){
   }
 }
 
-void loop(){
+void loop() {
 }
